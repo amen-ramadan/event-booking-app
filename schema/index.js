f872c6b97197caa390dce52b06b918be8c333b51
Amen Ramadan <ramadanamen407@gmail.com>
2025-08-04 23:47:49 +0300

    schema done

--- schema/index.js
+++ schema/index.js
@@ -3,7 +3,32 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 
 const typeDefs = `#graphql
   type Query {
-    greeting: String
+    events: [Event]
+    bookings: [Booking] #authenticated user bookings
+    getUserEvents(userId: ID!): [Event]
+  }
+
+  type Booking {
+    _id: ID!
+    event: Event!
+    createdAt: String!
+    user: User!
+    updatedAt: String!
+  }
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+  }
+
+  type Event {
+    _id: ID!
+    title: String!
+    description: String!
+    date: String!
+    price: Float!
+    creator: User!
   }
 `;
 
