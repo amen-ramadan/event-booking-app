533aca524ab771415372ce01102174e15003e175
Amen Ramadan <ramadanamen407@gmail.com>
2025-08-20 11:34:34 +0300

    inhance code before deploiment

--- front-end/src/api/queries.ts
+++ front-end/src/api/queries.ts
@@ -1,96 +1,99 @@
 import { gql } from "@apollo/client";
+import { EVENT_FIELDS } from "./fragments";
 
 export const EVENTS = gql`
-      query Events {
-        events {
-          _id
-          title
-          description
-          price
-          date
-          creator {
-            _id
-            username
-          }
-        }
+  ${EVENT_FIELDS}
+  query Events {
+    events {
+      ...EventFields
+      creator {
+        _id
+        username
       }
-    `;
+    }
+  }
+`;
 
 export const LOGIN = gql`
-      mutation Login($email: String!, $password: String!) {
-        login(email: $email, password: $password) {
-          token
-          userId
-          username
-        }
-      }
-    `;
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      userId
+      username
+    }
+  }
+`;
 
 export const CREATE_USER = gql`
-      mutation CreateUser($username: String!, $email: String!, $password: String!) {
-        createUser(userInput: { username: $username, email: $email, password: $password }) {
-          userId
-          token
-          username
-        }
-      }
-    `;
+  mutation CreateUser($username: String!, $email: String!, $password: String!) {
+    createUser(
+      userInput: { username: $username, email: $email, password: $password }
+    ) {
+      userId
+      token
+      username
+    }
+  }
+`;
 
 export const BOOK_EVENT = gql`
-      mutation BookEvent($eventId: ID!) {
-        bookEvent(eventId: $eventId) {
-          _id
-          createdAt
-          updatedAt
-        }
-      }
-    `;
+  mutation BookEvent($eventId: ID!) {
+    bookEvent(eventId: $eventId) {
+      _id
+      createdAt
+      updatedAt
+    }
+  }
+`;
 
 export const CREATE_EVENT = gql`
-      mutation CreateEvent($title: String!, $description: String!, $price: Float!, $date: String!) {
-        createEvent(eventInput: { title: $title, description: $description, price: $price, date: $date }) {
-          _id
-          title
-          description
-          price
-          date
-        }
+  ${EVENT_FIELDS}
+  mutation CreateEvent(
+    $title: String!
+    $description: String!
+    $price: Float!
+    $date: String!
+  ) {
+    createEvent(
+      eventInput: {
+        title: $title
+        description: $description
+        price: $price
+        date: $date
       }
-    `;
+    ) {
+      ...EventFields
+    }
+  }
+`;
 
 export const BOOKINGS = gql`
-      query Bookings {
-        bookings {
-          _id
-          createdAt
-          event {
-            _id
-            title
-            description
-            price
-            date
-          }
-        }
+  ${EVENT_FIELDS}
+  query Bookings {
+    bookings {
+      _id
+      createdAt
+      event {
+        ...EventFields
       }
-    `;
+    }
+  }
+`;
 
 export const CANCEL_BOOKING = gql`
-      mutation CancelBooking($bookingId: ID!) {
-        cancelBooking(bookingId: $bookingId) {
-          _id
-          title
-        }
-      }
-    `;
+  mutation CancelBooking($bookingId: ID!) {
+    cancelBooking(bookingId: $bookingId) {
+      _id
+      title
+    }
+  }
+`;
 
 export const EVENT_ADDED = gql`
-      subscription {
-        eventAdded {
-          _id
-          title
-          description
-          price
-          date
-        }
-      }
-    `;
+  ${EVENT_FIELDS}
+  subscription {
+    eventAdded {
+      ...EventFields
+    }
+  }
+`;
